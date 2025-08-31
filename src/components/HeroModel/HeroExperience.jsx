7449d0816b8951518c0c30fba3139ca4a617bee1
Dibyajyoti Newar <138105885+Dibyajyoti-newar@users.noreply.github.com>
2025-08-31 23:30:30 +0530

    Update HeroExperience.jsx

--- src/components/HeroModel/HeroExperience.jsx
+++ src/components/HeroModel/HeroExperience.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { OrbitControls } from '@react-three/drei'
 import { Canvas} from '@react-three/fiber';
 import {useMediaQuery} from "react-responsive"  
-import Particles from './particles';
+import Particles from './Particles';
 const HeroExperience = () => {
     const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
     const isTablet = useMediaQuery({ query: "(max-width: 1024px)" });  
