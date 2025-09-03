6d2ffe7b6cd18cf3b40059d076c3999b908a1aaf
Dibyajyoti Newar <138105885+Dibyajyoti-newar@users.noreply.github.com>
2025-09-03 15:41:48 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Hero from './sections/Hero'
-import NavBar from './components/NavBar'
+import NavBar from './components/Navbar'
 import Skills from './sections/Skills'
 import Experience from './components/Experience/Experience'
 import Certifications from './components/Certifications/Certifications'
